55348e7b98fd4336ec5dcbc6090231c7f7866084
Takumi Fukuzawa <forthune.0517@gmail.com>
2022-06-29 17:01:54 +0900

    QuizGroupのRecyclerViewを作成

--- app/src/main/java/com/fztkm/card/speedflashcard/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/fztkm/card/speedflashcard/screens/home/HomeViewModel.kt
@@ -1,6 +1,17 @@
 package com.fztkm.card.speedflashcard.screens.home
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.fztkm.card.speedflashcard.database.QuizDatabase
+import com.fztkm.card.speedflashcard.database.QuizGroup
 
-class HomeViewModel: ViewModel() {
+class HomeViewModel : ViewModel() {
+    private val _quizGroups = MutableLiveData<List<QuizGroup>>()
+    val quizGroups: LiveData<List<QuizGroup>>
+        get() = _quizGroups
+
+    init {
+        _quizGroups.value = QuizDatabase.getAllQuiz()
+    }
 }
